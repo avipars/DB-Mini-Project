66db49261bb07a59ce33652d1f277b40152a6a90
Avi <avipars@users.noreply.github.com>
2024-11-26 21:52:00 +0200

    moving stuff around

---
+++ Stage1/SelectAll.sql
@@ -0,0 +1,15 @@
+-- Get all entries - good for "backup"
+SELECT * FROM Book;
+SELECT * FROM Language;
+SELECT * FROM Location;
+SELECT * FROM Publisher;
+SELECT * FROM Author;
+SELECT * FROM Genre;
+SELECT * FROM Country;
+SELECT * FROM Written_By;
+SELECT * FROM Published_By;
+SELECT * FROM Written_In;
+SELECT * FROM Type_of;
+SELECT * FROM Is_In;
+
+commit;
