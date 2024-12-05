766aaf090dd32e3e4813af3982227f52cff4a22d
Leo Blam <leonard.blam@gmail.com>
2024-12-05 21:35:04 +0100

    added invalid constraints

--- Stage1/Index_Constraints/InvalidConstraints.sql
+++ Stage1/Index_Constraints/InvalidConstraints.sql
@@ -7,11 +7,11 @@ INSERT INTO Location (Quantity, Floor, Shelf, Location_ID, Condition, ID) VALUES
 -- Negative shelf number
 INSERT INTO Location (Quantity, Floor, Shelf, Location_ID, Condition, ID) VALUES (27, 'Adult Section', -52, 70001, 'New', 43003);
 -- Negative page count
-INSERT INTO Book (Title,ID , ISBN, Page_Count, Release_Date) VALUES ('The Hobbit',100000, 123456789, -310, '1937-09-21');
+INSERT INTO Book (Title, ID, Release_Date, Page_Count, Format, Description, ISBN) VALUES('Police ', 5000010, '1884-05-21', -39, 'Magazine', 'Market', 322397558);
 -- Duplicate ISBN
-INSERT INTO Book (Title, ID, Release_Date, Page_Count, Format, Description, ISBN) VALUES ('The Hobbit', 999990,  '1937-09-21', 5,'Journal','An epic journal',869427396);
+INSERT INTO Book (Title, ID, Release_Date, Page_Count, Format, Description, ISBN) VALUES('Police ', 5000010, '1884-05-21', 39, 'Magazine', 'Market', 322397088);
 -- Future release date
-INSERT INTO Book (ID, Title, ISBN, Page_Count, Release_Date) VALUES (1, 'The Hobbit', '123456789', 310, '2037-09-21');
+INSERT INTO Book (Title, ID, Release_Date, Page_Count, Format, Description, ISBN) VALUES('Police ', 5000011, '2025-05-21', 39, 'Magazine', 'Market', 322398088);
 -- Non-existent publisher
 INSERT INTO Published_By (ID, Publisher_ID) VALUES (66925, 38816);
 -- Future DOB
@@ -23,16 +23,33 @@ INSERT INTO Written_By (ID, Author_ID) VALUES (1, 818352457);
 -- These test should not work
 -- Update invalid constraints
 -- Negative book quantity
-UPDATE Location SET Quantity = -27 WHERE Location_ID = 70000;
+UPDATE Location SET Quantity = -27 WHERE Location_ID = 1;
 -- Negative shelf number
-UPDATE Location SET Shelf = -27 WHERE Location_ID = 70001;
+UPDATE Location SET Shelf = -27 WHERE Location_ID = 1;
 -- Duplicate ISBN
-UPDATE Book SET ISBN = 869427396 WHERE ID = 999990;
+UPDATE Book SET ISBN = 172100001 WHERE ID = 3;
 -- Update in written_by a null author
-UPDATE Written_By SET Author_ID = NULL WHERE ID = 1;
+UPDATE Written_By SET Author_ID = NULL WHERE Author_ID = 465532191;
+-- Update in written_by a non-existent author
+UPDATE Written_By SET Author_ID = 123456789 WHERE Author_ID = 465532191;
+-- Update in type_of a non-existent genre
+UPDATE Type_of SET Genre_ID = 123456789 WHERE Genre_ID = 465532191;
+-- Update in Publisher a not unique phone number
+UPDATE Publisher SET Phone_Number = '123456789' WHERE Publisher_ID = 1;
+
 
 -- Delete invalid constraints
 -- delete country that is in use
-DELETE FROM Is_In WHERE Country_ID = 1;
+DELETE FROM Country WHERE Country_ID = 23;
 -- delete author that is in use
-DELETE FROM Author WHERE Author_ID =1;
+DELETE FROM Author WHERE Author_ID =756070228;
+-- delete genre that is in use
+DELETE FROM Genre WHERE Genre_ID = 1;
+-- delete publisher that is in use
+DELETE FROM Publisher WHERE Publisher_ID = 1;
+-- delete language that is in use
+DELETE FROM Language WHERE Language_ID = 1;
+-- delete location that is in use
+DELETE FROM Location WHERE Location_ID = 1;
+-- delete book that is in use
+DELETE FROM Book WHERE ID = 1;
