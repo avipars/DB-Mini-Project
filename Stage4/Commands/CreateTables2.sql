7a63819f945322a58ef11a96d7f5f1946843d3c4
Avi <avipars@users.noreply.github.com>
2025-01-19 12:07:30 -0200

    combined schema

--- Stage4/Commands/CreateTables2.sql
+++ Stage4/Commands/CreateTables2.sql
@@ -1,3 +1,5 @@
+-- We combined the schema of both groups
+
 -- Creating language (of book) table
 CREATE TABLE IF NOT EXISTS Language
 (
@@ -36,115 +38,12 @@ CREATE TABLE IF NOT EXISTS Genre
   PRIMARY KEY (Genre_ID)
 );
 
-
 -- Creating country (of publisher)
 CREATE TABLE IF NOT EXISTS Country
 (
   Name VARCHAR(1000) NOT NULL,
   Country_ID INT NOT NULL,
   PRIMARY KEY (Country_ID)
-);
-
--- Creating employee table
-CREATE TABLE Employee
-(
-  Role VARCHAR(20) NOT NULL,
-  Age INT NOT NULL,
-  Salary INT NOT NULL,
-  Employee_ID INT NOT NULL,
-  Name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Employee_ID)
-);
-
--- Creating archive table
-CREATE TABLE Archive
-(
-  Archive_Number INT NOT NULL,
-  Book_Type VARCHAR(20) NOT NULL, -- govermental, public, private
-  PRIMARY KEY (Archive_Number)
-);
-
--- Creating is in table (publisher is situated in Country e.g. Random House is in the US)
-CREATE TABLE IF NOT EXISTS Is_In
-(
-  Publisher_ID INT NOT NULL,
-  Country_ID INT NOT NULL,
-  PRIMARY KEY (Publisher_ID, Country_ID),
-  FOREIGN KEY (Publisher_ID) REFERENCES Publisher(Publisher_ID) ON DELETE RESTRICT,
-  FOREIGN KEY (Country_ID) REFERENCES Country(Country_ID) ON DELETE RESTRICT
-);
-
--- Creating location (in library) table
-CREATE TABLE IF NOT EXISTS Location
-(
-  Quantity INT NOT NULL CHECK (Quantity > 0), 
-  Floor VARCHAR(1000) NOT NULL,
-  Shelf INT NOT NULL CHECK (Shelf >= 1),
-  Location_ID INT NOT NULL,
-  Condition VARCHAR(1000) NOT NULL,
-  ID INT NOT NULL,
-  Archive_Number INT,
-  PRIMARY KEY (Location_ID),
-  FOREIGN KEY (ID) REFERENCES Book(ID) ON DELETE RESTRICT,
-  FOREIGN KEY (Archive_Number) REFERENCES Archive(Archive_Number)
-);
-
-
-CREATE TABLE Book
-(
-  Title VARCHAR NOT NULL,
-  ID BIGINT NOT NULL,
-  Release_Date DATE NOT NULL,
-  Page_Count INT NOT NULL,
-  Format VARCHAR NOT NULL,
-  Description VARCHAR NOT NULL,
-  ISBN INT NOT NULL,
-  Rarity INT,
-  Location_ID INT NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (Location_ID) REFERENCES Location(Location_ID),
-  UNIQUE (ISBN)
-);
-
-CREATE TABLE Disposal
-(
-  Date INT NOT NULL,
-  Disposal_ID INT NOT NULL,
-  Material_of_Book INT NOT NULL,
-  Method INT NOT NULL,
-  ID BIGINT NOT NULL,
-  Employee_ID INT NOT NULL,
-  PRIMARY KEY (Disposal_ID),
-  FOREIGN KEY (ID) REFERENCES Book(ID),
-  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
-);
-
-CREATE TABLE Upkeep
-(
-  Upkeep_ID INT NOT NULL,
-  Tools_used INT NOT NULL,
-  Reason_for_Upkeep INT NOT NULL,
-  Date INT NOT NULL,
-  ID BIGINT NOT NULL,
-  Employee_ID INT NOT NULL,
-  PRIMARY KEY (Upkeep_ID),
-  FOREIGN KEY (ID) REFERENCES Book(ID),
-  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
-);
-
-CREATE TABLE Archival_Assignment
-(
-  Assignment_ID INT NOT NULL,
-  Date INT NOT NULL,
-  ID BIGINT NOT NULL,
-  Employee_ID INT NOT NULL,
-  Archive_Number INT NOT NULL,
-  New_LocationArchive_Number INT NOT NULL,
-  PRIMARY KEY (Assignment_ID),
-  FOREIGN KEY (ID) REFERENCES Book(ID),
-  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID),
-  FOREIGN KEY (Archive_Number) REFERENCES Archive(Archive_Number),
-  FOREIGN KEY (New_LocationArchive_Number) REFERENCES Archive(Archive_Number)
 );
 
 -- Creating written by table (author writes book)
@@ -185,4 +84,105 @@ CREATE TABLE IF NOT EXISTS Type_of
   PRIMARY KEY (ID, Genre_ID),
   FOREIGN KEY (ID) REFERENCES Book(ID) ON DELETE RESTRICT,
   FOREIGN KEY (Genre_ID) REFERENCES Genre(Genre_ID) ON DELETE RESTRICT
+);
+
+-- Creating is in table (publisher is situated in Country e.g. Random House is in the US)
+CREATE TABLE IF NOT EXISTS Is_In
+(
+  Publisher_ID INT NOT NULL,
+  Country_ID INT NOT NULL,
+  PRIMARY KEY (Publisher_ID, Country_ID),
+  FOREIGN KEY (Publisher_ID) REFERENCES Publisher(Publisher_ID) ON DELETE RESTRICT,
+  FOREIGN KEY (Country_ID) REFERENCES Country(Country_ID) ON DELETE RESTRICT
+);
+
+-- Creating location (in library) table
+CREATE TABLE IF NOT EXISTS Location
+(
+  Quantity INT NOT NULL CHECK (Quantity > 0), 
+  Floor VARCHAR(1000) NOT NULL,
+  Shelf INT NOT NULL CHECK (Shelf >= 1),
+  Location_ID INT NOT NULL,
+  Condition VARCHAR(1000) NOT NULL,
+  ID INT NOT NULL,
+  Archive_Number INT,
+  PRIMARY KEY (Location_ID),
+  FOREIGN KEY (ID) REFERENCES Book(ID) ON DELETE RESTRICT,
+  FOREIGN KEY (Archive_Number) REFERENCES Archive(Archive_Number)
+);
+
+CREATE TABLE Book
+(
+  Title VARCHAR(1000) NOT NULL,
+  ID BIGINT NOT NULL,
+  Release_Date DATE NOT NULL CHECK (Release_Date <= CURRENT_DATE),
+  Page_Count INT NOT NULL CHECK (Page_Count >= 1),
+  Format VARCHAR(1000) NOT NULL,
+  Description VARCHAR(1000) NOT NULL,
+  ISBN INT NOT NULL,
+  Rarity VARCHAR(30) NOT NULL,
+  Location_ID INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (Location_ID) REFERENCES Location(Location_ID),
+  UNIQUE (ISBN)
+);
+
+-- Creating employee table
+CREATE TABLE Employee
+(
+  Role VARCHAR(20) NOT NULL,
+  Age INT NOT NULL,
+  Salary INT NOT NULL,
+  Employee_ID INT NOT NULL,
+  Name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (Employee_ID)
+);
+
+-- Creating archive table
+CREATE TABLE Archive
+(
+  Archive_Number INT NOT NULL,
+  Book_Type VARCHAR(20) NOT NULL, -- govermental, public, private
+  PRIMARY KEY (Archive_Number)
+);
+
+CREATE TABLE Disposal
+(
+  Date DATE NOT NULL,
+  Disposal_ID INT NOT NULL,
+  Method VARCHAR(300) NOT NULL,
+  Material_of_Book VARCHAR(50) NOT NULL,
+  ID BIGINT NOT NULL,
+  Employee_ID INT NOT NULL,
+  PRIMARY KEY (Disposal_ID),
+  FOREIGN KEY (ID) REFERENCES Book(ID),
+  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
+);
+
+CREATE TABLE Upkeep
+(
+  Upkeep_ID INT NOT NULL,
+  Tools_used VARCHAR(300) NOT NULL,
+  Reason_for_Upkeep VARCHAR(300) NOT NULL,
+  Date DATE NOT NULL,
+  ID BIGINT NOT NULL,
+  Employee_ID INT NOT NULL,
+  PRIMARY KEY (Upkeep_ID),
+  FOREIGN KEY (ID) REFERENCES Book(ID),
+  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
+);
+
+CREATE TABLE Archival_Assignment
+(
+  Assignment_ID INT NOT NULL,
+  Date DATE NOT NULL,
+  ID BIGINT NOT NULL,
+  Employee_ID INT NOT NULL,
+  Archive_Number INT NOT NULL,
+  New_LocationArchive_Number INT NOT NULL,
+  PRIMARY KEY (Assignment_ID),
+  FOREIGN KEY (ID) REFERENCES Book(ID),
+  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID),
+  FOREIGN KEY (Archive_Number) REFERENCES Archive(Archive_Number),
+  FOREIGN KEY (New_LocationArchive_Number) REFERENCES Archive(Archive_Number)
 );
