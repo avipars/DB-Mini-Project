3c35d3fa3d8a593990b0a8694206903878b23eba
leib blam <leonard.blam@gmail.com>
2025-01-21 18:36:42 +0200

    did 1 query

---
+++ Stage4/Queries/ViewQueries.sql
@@ -0,0 +1,4 @@
+-- Query 1 Select 1:
+SELECT * FROM Find_Archived_Books_View WHERE release_date BETWEEN '2000-01-01' AND '2020-12-31' AND rarity = 'Legendary' AND book_type = 'Academic' AND floor = 'Study Area';
+
+-- Query 2 Select 2:
---
+++ Stage4/Views/Views.sql
@@ -0,0 +1,39 @@
+-- Create a view that shows all the archived books with their location and archive number
+CREATE OR REPLACE VIEW Find_Archived_Books_View AS
+SELECT 
+    B.Title, 
+    B.Release_Date, 
+    B.Rarity, 
+    A.Book_Type, 
+    L.Floor, 
+    L.Shelf, 
+    L.Condition,
+	A.Archive_Number
+FROM 
+    Book B
+JOIN 
+    Location L ON B.ID = L.ID
+JOIN 
+    Archive A ON L.Archive_Number = A.Archive_Number;
+
+-- Find all the employees that buried a book
+CREATE VIEW Buried_Books_Employees AS
+SELECT 
+    E.Employee_ID, 
+    E.Name, 
+    E.Role, 
+    E.Age, 
+    E.Salary, 
+    D.Date AS Disposal_Date, 
+    D.Method, 
+    D.Material_of_Book, 
+    B.Title AS Book_Title, 
+    B.ID AS Book_ID
+FROM 
+    Employee E
+JOIN 
+    Disposal D ON E.Employee_ID = D.Employee_ID
+JOIN 
+    Book B ON D.ID = B.ID
+WHERE 
+    D.Method = 'Buried';
